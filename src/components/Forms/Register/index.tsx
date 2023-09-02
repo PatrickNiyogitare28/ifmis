662d4220a1f7207a057dd2133b1cf952a640686b
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2023-09-02 18:29:13 +0200

    Added register

--- src/components/Forms/Register/index.tsx
+++ src/components/Forms/Register/index.tsx
@@ -3,12 +3,18 @@ import Input from '@/components/Elements/Input';
 import Link from 'next/link';
 import React from 'react';
 
-export default function LoginForm(){
+export default function RegisterForm(){
     return (
         <div className='bg-white rounded-xl w-full px-12 py-12'>
-            <h1 className='text-xl text-black font-bold'>Login to continue</h1>
+            <h1 className='text-xl text-black font-bold'>Create account</h1>
             <h3 className='text-gray-400 font-lighter'>Refugee Settlement System</h3>
             <div className='mt-8'>
+                <div className='mt-4'>
+                <Input placeholder='Full Name' type='text' />
+                </div>
+                <div className='mt-4'>
+                <Input placeholder='Phone number' type='text' />
+                </div>
                 <div className='mt-4'>
                 <Input placeholder='Email' type='text' />
                 </div>
@@ -19,7 +25,7 @@ export default function LoginForm(){
                     <FormActionButton label='SUBMIT' onSubmit={() => {}} />
                 </div>
                 <div className='mt-4'>
-                    <span className='text-gray-600'>Have no account? <Link href="/auth/signup" className='text-primary'>Create Account</Link></span>
+                    <span className='text-gray-600'>Have account? <Link href="/auth/signin" className='text-primary'>Login</Link></span>
                 </div>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Login from './auth/Login'
-
-
+import Login from './auth/signin';
 export default function Home() {
   return (
     <div>
