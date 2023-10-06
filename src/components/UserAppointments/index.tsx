1ba705a420f5172c5685e9dca56f6f3749e0aac6
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2023-10-06 15:35:32 +0200

    Added user reports

--- src/components/UserAppointments/index.tsx
+++ src/components/UserAppointments/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {BiPlus} from 'react-icons/bi';
+import {BiPlus, BiDownload} from 'react-icons/bi';
 import Modal from '../Modal';
 import AddAppointmentForm from '../Forms/AppointmentsForm';
 import { TSessionUser } from '@/types/user';
@@ -8,15 +8,21 @@ import UserAppointmentsTable from '../Elements/Tables/UserAppointmentsTable';
 
 export default function UserAppointmentsList ({User}: {User: TSessionUser}) {
     const [showModal, setShowModal] = useState<boolean>(false);
-    const {appointments, refetch} = useFetchUserAppointments({User});
+    const {appointments, refetch, handleDownloadReport} = useFetchUserAppointments({User});
     return (
         <>
         <div className='p-6'>
             <div className='w-full flex'>
+                <div className="ml-auto flex gap-4">
+                <button className='bg-primary flex gap-2 items-center px-6 rounded-xl py-2 ml-auto' onClick={handleDownloadReport}>
+                    <BiDownload color="white" />
+                    <label className='text-white font-light'>DOWNLOAD REPORT</label>
+                </button>
                 <button className='bg-primary flex gap-2 items-center px-6 rounded-xl py-2 ml-auto' onClick={() => setShowModal(true)}>
                     <BiPlus color="white" />
                     <label className='text-white font-light'>ADD NEW REQUEST</label>
                 </button>
+                </div>
             </div>
             <div className="mt-4">
             <UserAppointmentsTable appointments={appointments} />
